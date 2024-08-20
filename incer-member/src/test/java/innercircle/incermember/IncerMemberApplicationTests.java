733fc4e391a48bbf506b96f846ddc92e44ff8b73
suebinmon <jg0100290@gmail.com>
2024-08-20 21:44:48 +0900

    init: member init

---
+++ incer-member/src/test/java/innercircle/incermember/IncerMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package innercircle.incermember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncerMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
