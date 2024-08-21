a2991ff565b1b61cdc6355e9c791fcf22220a302
ksh <tjdgus2319@gmail.com>
2024-08-21 21:22:25 +0900

    init: celler init

---
+++ incer-celler/src/main/java/com/incerpay/incerceller/IncerCellerApplication.java
@@ -0,0 +1,13 @@
+package com.incerpay.incerceller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncerCellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncerCellerApplication.class, args);
+	}
+
+}
---
+++ incer-celler/src/test/java/com/incerpay/incerceller/IncerCellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incerpay.incerceller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncerCellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
