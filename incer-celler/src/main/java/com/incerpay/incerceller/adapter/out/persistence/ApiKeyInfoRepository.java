16e07b09c6df16803be5b7a293031e4d9b965014
kimsunghyun <tjdgus2319@gmail.com>
2024-09-10 17:27:32 +0900

    refactor: 패키지 구조 변경

--- incer-celler/src/main/java/com/incerpay/incerceller/adapter/out/persistence/ApiKeyInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.incerpay.incerceller.adapter.out.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApiKeyInfoRepository extends JpaRepository<ApiKeyInfoEntity, String> {
-
-}
